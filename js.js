20c042bb5e15f107fe204b886b575265b42ef6f2
Ferdi Demirci <38717493+ferdidemircitr@users.noreply.github.com>
2021-09-03 10:15:06 +0300

    product-card

---
+++ js.js
@@ -0,0 +1,33 @@
+const sizes = document.querySelectorAll('.size');
+const colors = document.querySelectorAll('.color');
+const shoes = document.querySelectorAll('.shoe');
+const gradients = document.querySelectorAll('.gradient');
+
+
+function changeSize() {
+    sizes.forEach(size => size.classList.remove('active'));
+    this.classList.add('active');
+
+}
+function changeColor() {
+    let primary = this.getAttribute('primary');
+    let color = this.getAttribute('color');
+    let shoe = document.querySelector(`.shoe[color = "${color}"]`);
+    let gradient = document.querySelector(`.gradient[color="${color}"]`);
+    
+
+    colors.forEach(color => color.classList.remove('active'));
+    this.classList.add('active');
+
+    document.documentElement.style.setProperty('--primary', primary);
+
+    shoes.forEach(s => s.classList.remove('show'));
+    shoe.classList.add('show');
+
+    gradients.forEach(g => g.classList.remove('first'));
+    gradient.classList.add('first');
+
+}
+
+sizes.forEach(size => size.addEventListener('click', changeSize));
+colors.forEach(color => color.addEventListener('click', changeColor));
